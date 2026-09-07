2be51b7717b9121b9e82f94ce016051bb57dbf89
CM6558 <161709237+CM6558@users.noreply.github.com>
2026-09-07 20:29:21 +0800

    Delete ksu-module directory

--- ksu-module/post-fs-data.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-# KernelSU post-fs-data hook: prepare private runtime directory only.
-MODDIR=${0%/*}
-RUNDIR=/data/adb/vtouch-merge
-mkdir -p "$RUNDIR" 2>/dev/null || exit 0
-chmod 0700 "$RUNDIR" 2>/dev/null || true
-# Do not start the merger here. service.sh starts it after input devices exist.
